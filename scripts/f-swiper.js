072b1ec73c1eaa13fafdffd184f6d893593865a2
Jhon Arias <87458097+jhons-dev@users.noreply.github.com>
2021-10-07 18:07:02 -0500

    scripts

---
+++ scripts/f-swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    spaceBetween: 12,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  
